2e41218bdecc5db2a97875936e0b239dd074304a
Vitaly Khabarov <vitkhab@gmail.com>
2017-10-13 19:26:38 +0300

    Draw arrows

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ dr = ImageDraw.Draw(im)
 
 fnt = ImageFont.truetype('SourceCodePro-Regular.ttf', 20)
 start = 50
-for step in steps:
+for i, step in enumerate(steps):
+    if i != len(steps) - 1:
+        arrow = (
+            (start+100, 40),
+            (start+150, 40),
+            (start+150, 25),
+            (start+200, 50),
+            (start+150, 75),
+            (start+150, 60),
+            (start+100, 60),
+            (start+100, 40)
+        )
+        dr.polygon(arrow, outline="black")
     dr.rectangle(((start,25),(start+100,75)), outline="black")
     dr.text((start+10, 35), step["name"], font=fnt, fill=(0,0,0))
     start += 200
