61e955b143b88d54d1da0862251cb69c5f410e80
Vitaly Khabarov <vitkhab@gmail.com>
2017-10-13 19:40:47 +0300

    Add atributes into image

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ steps = yaml.load(stream)
 
 size = len(steps)*200
 
-im = Image.new('RGB', (size,100), (255,255,255))
+im = Image.new('RGB', (size,200), (255,255,255))
 dr = ImageDraw.Draw(im)
 
 fnt = ImageFont.truetype('SourceCodePro-Regular.ttf', 20)
+attributes_font = ImageFont.truetype('SourceCodePro-Regular.ttf', 14)
 start = 50
 for i, step in enumerate(steps):
     if i != len(steps) - 1:
@@ -27,6 +28,10 @@ for i, step in enumerate(steps):
         dr.polygon(arrow, outline="black")
     dr.rectangle(((start,25),(start+100,75)), outline="black")
     dr.text((start+10, 35), step["name"], font=fnt, fill=(0,0,0))
+    dr.text((start+10, 80), "LT: {}".format(step["lead"]), font=attributes_font, fill=(0,0,0))
+    dr.text((start+10, 100), "VA: {}".format(step["value"]), font=attributes_font, fill=(0,0,0))
+    ca = "%C/A: {:.2%}".format(step["accepted"]/step["completed"])
+    dr.text((start+10, 120), ca, font=attributes_font, fill=(0,0,0))
     start += 200
 
 im.save("rectangle.png")
