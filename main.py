130278cf1cc9331efe0ea9f49f5a1368784d3a5b
Vitaly Khabarov <vitkhab@gmail.com>
2017-10-13 17:43:39 +0300

    Draw rectangle for each step

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import yaml
+from PIL import Image, ImageFont, ImageDraw
 
 filename = "examples/development.yml"
 stream = open(filename, "r")
 steps = yaml.load(stream)
+
+size = len(steps)*200
+
+im = Image.new('RGB', (size,100), (255,255,255))
+dr = ImageDraw.Draw(im)
+
+start = 50
+for step in steps:
+    dr.rectangle(((start,25),(start+100,75)), outline="black")
+    start += 200
+
+im.save("rectangle.png")
