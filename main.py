5a1374652ed09ab669adced64b79fdf977f86ff7
Vitaly Khabarov <vitkhab@gmail.com>
2017-10-13 19:15:47 +0300

    Add text into image

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ size = len(steps)*200
 im = Image.new('RGB', (size,100), (255,255,255))
 dr = ImageDraw.Draw(im)
 
+fnt = ImageFont.truetype('SourceCodePro-Regular.ttf', 20)
 start = 50
 for step in steps:
     dr.rectangle(((start,25),(start+100,75)), outline="black")
+    dr.text((start+10, 35), step["name"], font=fnt, fill=(0,0,0))
     start += 200
 
 im.save("rectangle.png")
